dc478a269a2213faf14cb48500c26bf81977a996
pourhadi <dan@pourhadi.com>
2025-02-10 06:50:02 +0000

    lkmdfslkdfm

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -4,9 +4,9 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceRoleKey) {
-  throw new Error("Missing Supabase environment variables" + supabaseUrl + ", " + supabaseAnonKey + ", " + supabaseServiceRoleKey)
-}
+// if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceRoleKey) {
+//   throw new Error("Missing Supabase environment variables" + supabaseUrl + ", " + supabaseAnonKey + ", " + supabaseServiceRoleKey)
+// }
 
 // Client for public usage (client-side)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
