fa88473ca435b96f7805e0062f0f9b5f482f2af9
pourhadi <dan@pourhadi.com>
2025-02-10 06:52:51 +0000

    dwdwdsa

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseUrl = "https://fdkdwoztozsqeythsffx.supabase.co/"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZka2R3b3p0b3pzcWV5dGhzZmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNTYwNDYsImV4cCI6MjA1NDczMjA0Nn0.32XwSScbULAVQALNAPsRF9-uDt5TcGAGHDFIThPBGww"
+const supabaseServiceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZka2R3b3p0b3pzcWV5dGhzZmZ4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczOTE1NjA0NiwiZXhwIjoyMDU0NzMyMDQ2fQ.WqNjbaH5dO4ef00i8qM4tQuXP7w2lfw6mMk47LS9Src"
 
 // if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceRoleKey) {
 //   throw new Error("Missing Supabase environment variables" + supabaseUrl + ", " + supabaseAnonKey + ", " + supabaseServiceRoleKey)
